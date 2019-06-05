61da143a653f1fc2f9bc2866a0baeaf7fe910aac
Faiq Allam <faiq.kaboel@gmail.com>
2019-06-05 11:51:54 +0700

    Add contact list and motto on footer

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -3,18 +3,59 @@
         <div class="container">
             <div class="columns">
                 <div class="column is-8-desktop is-offset-2-desktop">
-                    <p class="has-text-white">
-                        <small>
-                            <font-awesome-icon icon="copyright"></font-awesome-icon>
-                            2K19
-                            <strong class="has-text-white is-size-6">Kodeskillet</strong>
-                        </small>
-                    </p>
-                    <p class="mt-md">
-                        <a href="http://bulma.io">
-                            <img src="../../assets/made-with-bulma.png" alt="Made with Bulma" width="128" height="24">
-                        </a>
-                    </p>
+                    <div class="mb-xl">
+                        <h1 class="title is-3 has-text-light motto ff-space">
+                            Cook, Eat, Code, Repeat.
+                        </h1>
+                    </div>
+                    <div class="mb-lg">
+                        <b-tooltip label="Follow me on Twitter" type="is-light" animated>
+                            <a class="has-text-light mr-lg">
+                                <font-awesome-icon :icon="['fab', 'twitter']" class="fa-2x"></font-awesome-icon>
+                            </a>
+                        </b-tooltip>
+                        <b-tooltip label="Add me on Facebook" type="is-light" animated>
+                            <a class="has-text-light mr-lg">
+                                <font-awesome-icon :icon="['fab', 'facebook']" class="fa-2x"></font-awesome-icon>
+                            </a>
+                        </b-tooltip>
+                        <b-tooltip label="Look me up on LinkedIn" type="is-light" animated>
+                            <a class="has-text-light mr-lg">
+                                <font-awesome-icon :icon="['fab', 'linkedin']" class="fa-2x"></font-awesome-icon>
+                            </a>
+                        </b-tooltip>
+                        <b-tooltip label="Chat me on Whatsapp" type="is-light" animated>
+                            <a class="has-text-light mr-lg">
+                                <font-awesome-icon :icon="['fab', 'whatsapp']" class="fa-2x"></font-awesome-icon>
+                            </a>
+                        </b-tooltip>
+                        <b-tooltip label="Visit my GitHub" type="is-light" animated>
+                            <a class="has-text-light mr-lg">
+                                <font-awesome-icon :icon="['fab', 'github']" class="fa-2x"></font-awesome-icon>
+                            </a>
+                        </b-tooltip>
+                        <b-tooltip label="Mail to kaboel@kodeskillet.com" type="is-light" animated>
+                            <a class="has-text-light">
+                                <font-awesome-icon :icon="['fas', 'envelope']" class="fa-2x"></font-awesome-icon>
+                            </a>
+                        </b-tooltip>
+                    </div>
+                    <div class="mb-lg">
+                        <p class="has-text-white">
+                            <small>
+                                <font-awesome-icon icon="copyright"></font-awesome-icon>
+                                2K19. All Rights Reserved.
+                            </small>
+                        </p>
+                        <p>
+                            <strong class="has-text-white is-size-7">
+                                kaboel@kodeskillet.com
+                            </strong>
+                        </p>
+                    </div>
+                    <div>
+                        <img src="../../assets/made-with-bulma.png" width="150">
+                    </div>
                 </div>
             </div>
         </div>
@@ -22,7 +63,9 @@
 </template>
 
 <script>
+    import BTooltip from "buefy/src/components/tooltip/Tooltip";
     export default {
-        name: 'Footer'
+        name: 'Footer',
+        components: {BTooltip},
     }
 </script>
