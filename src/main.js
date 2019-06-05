d703897f55e09df0ecb665fc4d77a5f71fc47b1c
Faiq Allam <faiq.kaboel@gmail.com>
2019-06-05 09:20:51 +0700

    Import font awesome icons

--- src/main.js
+++ src/main.js
@@ -1,21 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import {library} from "@fortawesome/fontawesome-svg-core";
-import {faCopyright, faEnvelope} from "@fortawesome/free-solid-svg-icons";
+
+//Fa Solid
+import {
+    faCopyright, faEnvelope, faGlobe, faCode, faHeart
+} from "@fortawesome/free-solid-svg-icons";
+library.add(
+    faEnvelope, faCopyright, faGlobe, faCode, faHeart
+);
+
+//Fa Regular
+import {
+    faSmileWink
+} from "@fortawesome/free-regular-svg-icons";
+library.add(
+    faSmileWink
+);
 
 Vue.config.productionTip = false;
 
 Vue.use(Buefy);
-
 Vue.component('font-awesome-icon', FontAwesomeIcon);
-library.add(
-    faEnvelope, faCopyright
-);
 
 new Vue({
     router,
