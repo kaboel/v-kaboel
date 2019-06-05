75b7be9162f46f3b223614053eec9c0156870804
Faiq Allam <faiq.kaboel@gmail.com>
2019-06-05 13:07:39 +0700

    Test build 5

---
+++ docs/js/chunk-2d0a4d7f.ed1bb554.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0a4d7f"],{"07bd":function(e,t,n){"use strict";n.r(t);var c=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{attrs:{id:"project"}},[n("h1",[e._v("This is the project page")])])}],a=n("0284"),o={name:"project",methods:{track:function(){Object(a["page"])("/project")}}},s=o,i=n("2877"),u=Object(i["a"])(s,c,r,!1,null,null,null);t["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d0a4d7f.ed1bb554.js.map
