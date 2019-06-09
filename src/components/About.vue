f5fe26567aab60cb76a0ec181c1e6d78312c26a0
Faiq Allam <faiq.kaboel@gmail.com>
2019-06-09 19:04:34 +0700

    Add About component

---
+++ src/components/About.vue
@@ -0,0 +1,33 @@
+<template>
+    <section id="about" class="hero is-medium">
+        <div class="hero-body has-background-black has-text-centered">
+            <div class="columns pb-lg">
+                <div class="column is-8-desktop is-offset-2-desktop">
+                    <h1 class="title is-size-4 has-text-light">
+                        Hey, I'm Faiq. It's very nice to meet you.
+                    </h1>
+                    <h2 class="subtitle is-size-5 has-text-light mt-lg">
+                        I'm a Full Stack web developer, design and build Websites and Web Apps.
+                        I have a very high level of curiosity, which maybe dangerous but also can be handy when it comes to a complex project.
+                        I'm also a confident, eager to find out something new, and perpetually improving
+                        my skill set in what I love, The Web. I am currently pursuing a diploma of Technical Information
+                        at State Polytechnic of Malang, and will graduate in 2023.
+                    </h2>
+                </div>
+            </div>
+        </div>
+        <div class="hero-foot">
+            <div class="columns" style="margin-top: -7.5rem;">
+                <div class="column is-10-desktop is-offset-1-desktop">
+
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'about'
+    }
+</script>
