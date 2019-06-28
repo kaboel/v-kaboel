d62d06d3984a945cb24c6c9fbdb876e62d7081f4
Faiq <faiq.kaboel@gmail.com>
2019-06-28 07:13:56 +0700

    Re-design 404 not found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,28 +1,34 @@
 <template>
-    <div id="not-found">
-        <div class="container">
-            <div class="columns">
-                <div class="column is-8-desktop is-offset-2-desktop mt-lg">
-                    <div class="hero">
-                        <div class="hero-body has-text-centered has-text-black pt-sm ">
-                            <p class="title is-xl watermark">404</p>
-                            <img src="../assets/KS-logo-black-400.png" class="watermark">
-                            <hr>
-                            <p class="subtitle is-3 mt-lg">
-                                I don't think this is the page you looking for.
-                            <br>
-                                Let's get you back on track !
+    <section id="not-found" class="hero is-fullheight has-text-centered">
+        <div class="hero-body has-text-black pt-sm ">
+            <div class="container">
+                <div class="columns is-centered">
+                    <div class="column is-three-fifths">
+                        <div class="main">
+                            <p class="title is-xl">
+                                404
                             </p>
-                            <router-link class="button is-black is-outlined is-rounded"
-                                         to="/">
-                                <strong>Click me !</strong>
-                            </router-link>
+                            <p class="title is-1 ">
+                                Not Found
+                            </p>
                         </div>
+                        <p class="title is-3 has-text-white">
+                            Whoops! There's nothing here...
+                        </p>
+                        <p class="subtitle is-5 mt-lg">
+                            Trust me, you dont want to be here.
+                            <br>
+                            Let's get you back on track.
+                        </p>
+                        <router-link class="button is-white is-outlined is-rounded mt-lg"
+                                     to="/">
+                            <strong>Get me out of here !</strong>
+                        </router-link>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 </template>
 
 <script>
@@ -32,18 +38,37 @@
 </script>
 
 <style scoped>
-    .is-xl {
-        font-size: 12rem;
+    #not-found {
+        background-color: #0A0A0A;
     }
-    .title {
+    .main {
+        max-width: 400px;
+        margin: auto;
+    }
+    .title.is-xl {
         font-style: italic;
-        letter-spacing: 1rem;
+        font-size: 12rem;
+        -webkit-text-fill-color: #0A0A0A;
+        -webkit-text-stroke-width: 7px;
+        -webkit-text-stroke-color: #202020;
     }
-    .title,  hr {
+    .title.is-1 {
+        text-align: left;
+        margin-top: -3rem !important;
+        letter-spacing: .0rem;
+        color: #4f4f4f;
+        font-style: italic;
+        font-weight: bolder !important;
+    }
+    .title.is-3 {
+        margin-top: 5rem !important;
+    }
+    .title{
         margin: 0 !important;
     }
-    .watermark{
-        filter:opacity(15%);
-        margin-top: -1rem;
+    p.is-5, .subtitle {
+        color: #fff;
+        line-height: 2rem;
+        letter-spacing: 2px;
     }
 </style>
